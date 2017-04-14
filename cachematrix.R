8e4813c71017a459dc548a60dee66ff3304c0ce7
Umesh <monukaushik@gmail.com>
2017-04-14 08:05:47 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Coursera Data Science: R Programming, Assignment Week 3, 
+## Git Hub user: UMESH K
 
-## Write a short comment describing this function
+## This function creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-  inv <- NULL
+ inv_var <- NULL                                ## to hold value of matrix inverse 
+    set_fun <- function(y) {                    ## defining the set_fun function to assign new 
+        x <<- y                                 ## value of matrix in parent environment
+        inv_var <<- NULL                        ## if there is a new matrix, reset inv_var to NULL
+    }
+    get_fun <- function() x                     ## define the get_fun fucntion - returns value of the matrix argument
+    
+    setinverse <- function(inverse) inv_var <<- inverse  ## assigns value of inv_var in parent environment
+    getinverse <- function() inv_var                     ## gets the value of inv_var where called
+    list(set_fun = set_fun, get_fun = get_fun, setinverse = setinverse, getinverse = getinverse)  
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above.
+## If the inverse has already been calculated (and the matrix has not changed),
+## then cacheSolve will retrieve the inverse from the cache
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...) {         ## Return a matrix that is the inverse of 'x'
+  inv_var <- x$getinverse()
+     if(!is.null(inv_var)) {             ## Check if inv_var is already available in current environment
+         message("getting cached data")
+         return(inv_var)
+     }
+     data <- x$get_fun()
+     inv_var <- solve(data, ...)
+     x$setinverse(inv_var)
+     inv_var
 }
